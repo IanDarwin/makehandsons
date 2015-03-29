f8f370a5eea880e9310bfa7f2676223b49119437
Ian Darwin <ian@darwinsys.com>
2015-03-29 08:56:03 -0400

    Start adding tests for actual processing, now that it's exposed for testing.

--- src/test/java/makehandsons/TestSomeUtilities.java
+++ src/test/java/makehandsons/TestSomeUtilities.java
@@ -12,11 +12,15 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestSomeUtilities {
+	TextModes modes;
 	MakeHandsOns target;
+	
+	final static File inputFile = new File("test file");
 
 	@Before
 	public void mehtUp() {
 		target = new MakeHandsOns();
+		modes = new TextModes();
 	}
 
 	@Test
@@ -27,15 +31,26 @@ public class TestSomeUtilities {
 		assertFalse(target.isTextFile("Llewelynn.class"));
 	}
 	
-	final static File inputFile = new File("test file");
-	
 	@Test
 	public void testProcessTextFileLinesIdemPotent() {
 		List<String> input = Arrays.asList("One","Two");
-		TextModes modes = new TextModes();
+		
 		List<String> output = target.processTextFileLines(input, inputFile, modes);
 		for (int i = 0; i < output.size(); i++) {
 			assertEquals(input.get(i), output.get(i));
 		}
+	}
+	
+	@Test
+	public void testCutMode() {
+		List<String> input = Arrays.asList(
+			"//-",
+			"/* This should not appear in the output */",
+			"//+",
+			"int i = 0;" // This is the online line that should appear
+			);
+		List<String> output = target.processTextFileLines(input, inputFile, modes);
+		assertEquals(1, output.size());
+		assertFalse(output.toString().contains("should not appear"));
 	}
 }
