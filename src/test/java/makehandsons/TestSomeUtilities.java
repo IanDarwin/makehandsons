4910b58a01a5a3badc4f2123a39b9f43109e80af
Ian Darwin <ian@darwinsys.com>
2018-08-28 18:28:07 -0400

    Imports police

--- src/test/java/makehandsons/TestSomeUtilities.java
+++ src/test/java/makehandsons/TestSomeUtilities.java
@@ -1,12 +1,7 @@
 package makehandsons;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
-import java.io.File;
-import java.util.Arrays;
-import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
