7d00e598f7be39944363d05a2a6a1d8d4ac7ff31
Ian Darwin <ian@darwinsys.com>
2022-09-22 14:57:24 +0100

    Start adding 'nonSolution' words

--- src/main/java/makehandsons/MakeHandsOns.java
+++ src/main/java/makehandsons/MakeHandsOns.java
@@ -73,6 +73,9 @@ public class MakeHandsOns {
 
 	/** Map from a compiled regex Pattern to its replacement String */
 	static Map<Pattern,String> pattMap;
+
+	/** List of exceptions that contain "solution" but must not be converted */
+	List<String> nonSolutions = List.of("dependencyResolutionManagement");
 	
 	/** JUL logger */
 	private static Logger log;
